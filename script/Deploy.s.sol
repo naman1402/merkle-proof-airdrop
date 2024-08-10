1ef79df09f53f1d6677e6ce60adae6d2bb6de48a
naman mohnani <mohnaninaman@gmail.com>
2024-08-11 01:08:12 +0530

    deploying...

---
+++ script/Deploy.s.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import {Token} from "../src/Token.sol";
+import {Airdrop} from "../src/Airdrop.sol";
+import {console} from "forge-std/console.sol";
+import {Script} from "forge-std/Script.sol";
+
+contract Deploy is Script {}
---
+++ test/Airdrop.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import {Test} from "forge-std/Test.sol";
+import {console} from "forge-std/console.sol";
+import {Token} from "../src/Token.sol";
+import {Airdrop} from "../src/Airdrop.sol";
+
+contract Airdrop is Test {}
