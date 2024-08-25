d033769ffe34b74dea0c8736a77af86d25b1405b
naman mohnani <mohnaninaman@gmail.com>
2024-08-25 18:33:42 +0530

    completing airdrop contract

--- src/Airdrop.sol
+++ src/Airdrop.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
+/// @notice MerkleProof is used to verify if given leaf node is part of MerkleTree, EIP712 provides utilities for creating structured message signatures
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
@@ -14,7 +15,9 @@ error Airdrop__AlreadyClaimed();
 error Airdrop__InvalidSignature();
 
 /// @title Merkle Airdrop - Airdrop tokens to users who can prove they are in merkle tree
+
 contract Airdrop is EIP712 {
+
     /// @dev Elliptic Curve Digital Signature Algorithm: functions are used to verify that a message was signed by the holder of the private keys of a given address
     /// @dev prevent sending token to address who can't receive using SafeERC20
     using ECDSA for bytes32;
@@ -23,9 +26,9 @@ contract Airdrop is EIP712 {
     event Claimed(address account, uint256 amount);
     event MerkleRootUpdated(bytes32 newRoot);
 
-    IERC20 private immutable airdropToken;
-    bytes32 private immutable merkleRoot;
-    mapping(address => bool) private hasClaimed;
+    IERC20 private immutable airdropToken; // token being airdropped
+    bytes32 private immutable merkleRoot; // state variable that holds merkle root
+    mapping(address => bool) private hasClaimed; // mapping to check if an address has already claimed or not
     bytes32 private constant MESSAGE = keccak256("AirdropClaim(address account, uint256 amount)");
 
     struct AirdropClaim {
@@ -38,10 +41,9 @@ contract Airdrop is EIP712 {
         airdropToken = _airdropToken;
     }
 
-    ///////////////// FUNCTIONS
-    /// @dev verifies if claims is not made yet, validates the provided signature and merkle proof, and finally transfers the airdrop token
-    /// @param v, r, s are ethereum ECDSA singature components
-
+    /// @notice leaf is the node in the merkle tree for user's claim, calling MerkleProof verify function to check if it is valid, reverts for failure
+    /// if proof is correct, change internal mapping and transfer erc20 token
+    /// @notice isValidSignature - here the signer is account and data is the hash generated using EIP712 function
     function claim(address account, uint256 amount, bytes32[] calldata merkleProof, uint8 v, bytes32 r, bytes32 s)
         external
     {
@@ -63,17 +65,12 @@ contract Airdrop is EIP712 {
         airdropToken.safeTransfer(account, amount);
     }
 
-    // signed message we expect from user
-    /// @dev main objective is used to create hash of structured messageused for signing or verification purpose
-    //
-    ///* @dev keccak256 is cryptographic hash used to create 256-bit hash of data
-    // abi.encode(); encode data into binary format suitable for eth transaction,
-
+    /// @notice this generated a hash that represents structural message, first we encode the message then hash it using keccak256 function
     function getMessageHash(address account, uint256 amount) public view returns (bytes32) {
         return _hashTypedDataV4(keccak256(abi.encode(MESSAGE, AirdropClaim({account: account, amount: amount}))));
     }
 
-    /////////////////// VIEW FUNCTIONS
+    /////////////////// VIEW FUNCTIONS ////////////////////// 
     function getAirdropToken() external view returns (IERC20) {
         return airdropToken;
     }
@@ -82,6 +79,8 @@ contract Airdrop is EIP712 {
         return merkleRoot;
     }
 
+    /// @notice check if given signature valid for given message and signer, returns true if recovered address matches the sign, or else false
+    /// EIP712 is designed to make it easier to verify that a message was signed by a specific user in dApp, 
     function isValidSignature(address signer, bytes32 digest, uint8 _v, bytes32 _r, bytes32 _s)
         internal
         pure
